b49b0821a290708418b45c29e024ad7757931475
unknown <gercidio.valeriano@gmail.com>
2023-11-11 16:54:26 -0300

    revisão do conteúdo

--- aula_03/src/aula_03/Atividade2.java
+++ aula_03/src/aula_03/Atividade2.java
@@ -7,6 +7,21 @@ public class Atividade2 {
 
 	public static void main(String[] args) {
 		
+		int numero;
+		
+		System.out.println("Digite um número: ");
+		numero = leia.nextInt();
+		
+		if (numero >= 0 && (numero%2 <= 0)){
+			System.out.println("O número  " +  numero + " é positivo e par!");
+		}else if(numero >= 0 && (numero%2 <=1)) {
+			System.out.println("O número  " +  numero + " é positivo e impar!");
+		}else if(numero <= 0 && (numero%2 <= 0)) {
+			System.out.println("O número  " +  numero + " é negativo e par!");
+		}else if(numero <= 0 && (numero%2 <= 1)) {
+			System.out.println("O número  " +  numero + " é negativo e impar!");
+
+		}
 		
 	}
 
