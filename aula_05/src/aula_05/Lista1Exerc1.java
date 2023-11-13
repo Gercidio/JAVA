5a61d068fe7c0002572dbc81436e876a15aaa6ee
unknown <gercidio.valeriano@gmail.com>
2023-11-13 16:30:39 -0300

    atividades aula 5

--- aula_05/src/aula_05/Lista1Exerc1.java
+++ aula_05/src/aula_05/Lista1Exerc1.java
@@ -7,26 +7,25 @@ public class Lista1Exerc1 {
 
 	public static void main(String[] args) {
 		
-		int[] numeroInt = {12,54,23,234,87,6,7,8,9,10};
-		int numero;
+		int[] numeroInt = {12,54,23,234,87,6,7,8,9,10}; //vetor
+		int numero, posicao = -1; //variaveis para saber o numero pesquisado e a posição
 		
-		for(int indice = 0; indice < numeroInt.length; indice++) {
+			
 		System.out.println("Pesquise um número: ");
 		numero = leia.nextInt();
 
-		int posicao = -1;
-		for(int i = 0; i < numeroInt.length; i++) {
+		for(int i = 0; i < numeroInt.length; i++) { //se o numero digitado for igual ao encontrado, armazena na posição i e retorna o valor no final
 			if(numeroInt[i] == numero) {
 				posicao = i;
 				break;
-			}
+			
 		}
 		if(posicao!= -1) {
 			System.out.println("O numero " + numero + " foi encontrado na posição " + posicao);
-			break;
+			
 		}else {
-			System.out.println("O numero " + numero + " não foi encontrado!");
-			break;
+			System.out.println("O numero " + numero + " não foi encontrado!"); //caso nao seja encontrado, exibe a msg
+			
 		}
 		
 		}
---
+++ aula_05/src/aula_05/Lista1Exerc2.java
@@ -0,0 +1,14 @@
+package aula_05;
+
+import java.util.Scanner;
+
+public class Lista1Exerc2 {
+	static Scanner leia = new Scanner(System.in);
+	public static void main(String[] args) {
+
+		int[] numeroInt = {12,51,23,234,87,6,7,8,9,10};
+		
+		
+	}
+
+}
